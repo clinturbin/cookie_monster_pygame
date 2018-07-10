0981304df2a60d48583109400eae34bfe86ba1c5
clinturbin <clinturbin@gmail.com>
2018-07-10 09:10:45 -0400

    Both bomb and cookie images now fall from top of screen.

--- cookie_monster.py
+++ cookie_monster.py
@@ -1,11 +1,11 @@
 import pygame, sys, random
-from pygame.locals import *
 
-class Cookie(object):
-    def __init__(self, screen_width, screen_height):
+class Falling_Object(object):
+    def __init__(self, screen_width, screen_height, item_type):
         self.size = 30
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.type = item_type
         self.x_pos = random.randint(0, self.screen_width - self.size) 
         self.y_pos = (0 - self.size)
         self.item_drop_speed = random.randint(1, 8)
@@ -13,8 +13,14 @@ class Cookie(object):
     def update_y_position(self):
         self.y_pos += self.item_drop_speed
 
+    def get_image(self):
+        if self.type == 'cookie':
+            return pygame.image.load('images/cookie3.png').convert_alpha()
+        elif self.type == 'bomb':
+            return pygame.image.load('images/bomb.png').convert_alpha()
+
     def render_image(self, screen):
-        image = pygame.image.load('images/cookie3.png').convert_alpha()
+        image = self.get_image()
         transformed_image = pygame.transform.scale(image, (self.size, self.size))
         image_rect = pygame.Rect(self.x_pos, self.y_pos, self.size, self.size)
         screen.blit(transformed_image, image_rect)
@@ -25,16 +31,16 @@ def main():
     screen_width = 900
     screen_height = 700
     
-    # Set up colors
-    blue = (0, 0, 255)
-    gray = (128, 128, 128)
-
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
     game_clock = pygame.time.Clock()
     pygame.display.set_caption('Cookie Monster')
 
-    cookies = []
+    # Set up colors
+    blue = (0, 0, 255)
+    gray = (128, 128, 128)
+
+    falling_objects = []
 
     item_drop_rate = 10
     item_drop_counter = 0
@@ -49,18 +55,21 @@ def main():
 
         if item_drop_counter == item_drop_rate:
             item_drop_counter = 0
-            cookies.append(Cookie(screen_width, screen_height))
+            item_type = 'bomb'
+            if random.randint(1, 3) == 1:
+                item_type = 'cookie'
+            falling_objects.append(Falling_Object(screen_width, screen_height, item_type))
 
-        for cookie in cookies[:]:
-            cookie.update_y_position()
-            if cookie.y_pos > screen_height:
-                cookies.remove(cookie)
+        for item in falling_objects[:]:
+            item.update_y_position()
+            if item.y_pos > screen_height:
+                falling_objects.remove(item)
 
 
         screen.fill((gray))
 
-        for cookie in cookies:
-            cookie.render_image(screen)
+        for item in falling_objects:
+            item.render_image(screen)
 
         pygame.display.update()
         game_clock.tick(60)
