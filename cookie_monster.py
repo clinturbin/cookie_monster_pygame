4499e14fad9ee60695ad4cd606bbff42129c3329
clinturbin <clinturbin@gmail.com>
2018-07-10 09:38:42 -0400

    added player image to screen and ability to move image using mousemotion event type.

--- cookie_monster.py
+++ cookie_monster.py
@@ -1,4 +1,5 @@
 import pygame, sys, random
+from pygame.locals import *
 
 class Falling_Object(object):
     def __init__(self, screen_width, screen_height, item_type):
@@ -35,6 +36,12 @@ def main():
     screen = pygame.display.set_mode((screen_width, screen_height))
     game_clock = pygame.time.Clock()
     pygame.display.set_caption('Cookie Monster')
+    pygame.mouse.set_visible(False)
+
+    # set up the player image
+    player_image = pygame.image.load('images/cookie_monster.png').convert_alpha()
+    player_rect = player_image.get_rect()
+    player_rect.topleft = (screen_width / 2, screen_height - 100)
 
     # Set up colors
     blue = (0, 0, 255)
@@ -53,6 +60,14 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
+            if event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    done = True
+
+            if event.type == MOUSEMOTION:
+                player_rect.move_ip(event.pos[0] - player_rect.centerx, event.pos[1] - player_rect.centery)
+
+
         if item_drop_counter == item_drop_rate:
             item_drop_counter = 0
             item_type = 'bomb'
@@ -67,10 +82,11 @@ def main():
 
 
         screen.fill((gray))
-
+        # Draw the player's rectangle
+        screen.blit(player_image, player_rect)
+        # draw falling objects to screen
         for item in falling_objects:
             item.render_image(screen)
-
         pygame.display.update()
         game_clock.tick(60)
 
