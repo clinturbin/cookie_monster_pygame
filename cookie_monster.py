cb8b0f3dae47d8b1e0a169450dafe451fb872159
clinturbin <clinturbin@gmail.com>
2018-07-11 14:29:49 -0400

    Added Levels to increase item drop rate as game progresses.

--- cookie_monster.py
+++ cookie_monster.py
@@ -22,7 +22,17 @@ start_game_sound = pygame.mixer.Sound('images/cookies.wav')
 hello_sound = pygame.mixer.Sound('images/hello.wav')
 pygame.mixer.music.load('images/c_is_for_cookie_3.wav')
 
+cookie_monster_player_img = pygame.image.load('images/cookie_monster.png').convert_alpha()
+cookie_monster_start_img = pygame.image.load('images/cm1.png').convert_alpha()
+
 font = pygame.font.Font('freesansbold.ttf', 20)
+
+LEVELS = {
+            1:{'cookie_drop_rate': 50, 'min_drop_speed': 1, 'max_drop_speed': 8, 'item_drop_rate': 10, 'next_level_score': 25},
+            2:{'cookie_drop_rate': 40, 'min_drop_speed': 2, 'max_drop_speed': 12, 'item_drop_rate': 6, 'next_level_score': 40},
+            3:{'cookie_drop_rate': 30, 'min_drop_speed': 4, 'max_drop_speed': 14, 'item_drop_rate': 4, 'next_level_score': 50}
+}
+
 
 class Falling_Object(object):
     def __init__(self, item_type):
@@ -56,13 +66,14 @@ class Falling_Object(object):
         screen.blit(transformed_image, image_rect)
 
 class Player(object):
-    def __init__(self):
-        self.image = pygame.image.load('images/cookie_monster.png').convert_alpha()
+    def __init__(self, image):
+        self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100)
         self.score = 0
         self.top_score = 0
         self.lives = 5
+        self.level = 1
 
     def update_score(self, falling_item_hit):
         self.play_sound_effect(falling_item_hit)
@@ -72,14 +83,12 @@ class Player(object):
             self.reset_score()
             self.decrease_lives()
         
-
     def play_sound_effect(self, falling_item_hit):
         if falling_item_hit.item_type == 'cookie':
-            if random.randint(1, 100) < 30: # only want cookie sound to play 30% of time
+            if random.randint(1, 100) <= 30: # only want cookie sound to play 30% of time
                 eat_cookie_sound.play()
         else:
             bomb_sound.play()
-
     
     def increase_score(self):
         self.score += 1
@@ -93,13 +102,22 @@ class Player(object):
         self.lives -= 1
         if self.lives == 0:
             game_over()
+
+    def get_game_level(self):
+        if self.level == 1 and self.score >= LEVELS[self.level]['next_level_score']:
+            self.level = 2
+        if self.level == 2 and self.score >= LEVELS[self.level]['next_level_score']:
+            self.level = 3
+        if self.level == 3 and self.score == LEVELS[self.level]['next_level_score']:
+            game_over()
+        return self.level
     
 
 def quit_game():
     pygame.quit()
     sys.exit()    
 
-def add_text(text, font, surface, x, y, color):
+def add_score_text(text, font, surface, x, y, color):
     text_obj = font.render(text, 1, color)
     text_rect = text_obj.get_rect()
     text_rect.topright = (x, y)
@@ -108,10 +126,10 @@ def add_text(text, font, surface, x, y, color):
 def load_screen():
     hello_sound.play()
 
-    start_screen_image = pygame.image.load('images/cm1.png').convert_alpha()
-    start_screen_image_rect = start_screen_image.get_rect()
-    start_screen_image_rect.bottom = (SCREEN_HEIGHT / 10) * 7
-    start_screen_image_rect.centerx = (SCREEN_WIDTH / 2)
+    image = cookie_monster_start_img
+    image_rect = image.get_rect()
+    image_rect.bottom = (SCREEN_HEIGHT / 10) * 7
+    image_rect.centerx = (SCREEN_WIDTH / 2)
 
     button = pygame.Rect(150, (SCREEN_HEIGHT / 10) * 7, (SCREEN_WIDTH / 10) * 6, 50)
     text_obj = font.render("PLAY!!", 1, WHITE)
@@ -137,7 +155,7 @@ def load_screen():
                     done = True
 
         screen.fill((BLACK))
-        screen.blit(start_screen_image, start_screen_image_rect)
+        screen.blit(image, image_rect)
         pygame.draw.rect(screen, BLUE, button)
         screen.blit(text_obj, text_rect)
         pygame.display.update()
@@ -147,14 +165,16 @@ def load_screen():
 def play_game():
     pygame.mixer.music.play(-1, 0.0)
     pygame.mouse.set_visible(False)
-    cookie_monster = Player()
+
+    player = Player(cookie_monster_player_img)
     falling_objects = []
 
-    item_drop_rate = 10
     item_drop_counter = 0
 
     done = False
     while not done:
+        current_level = player.get_game_level()   # gets the current level
+        item_drop_rate = LEVELS[current_level]['item_drop_rate']
         item_drop_counter += 1
 
         for event in pygame.event.get():
@@ -166,12 +186,12 @@ def play_game():
                     quit_game()
 
             if event.type == MOUSEMOTION:
-                cookie_monster.rect.move_ip(event.pos[0] - cookie_monster.rect.centerx, event.pos[1] - cookie_monster.rect.centery)
+                player.rect.move_ip(event.pos[0] - player.rect.centerx, event.pos[1] - player.rect.centery)
 
         if item_drop_counter == item_drop_rate:
             item_drop_counter = 0
             item_type = 'bomb'
-            if random.randint(1, 3) == 1:
+            if random.randint(1, 100) <= LEVELS[current_level]['cookie_drop_rate']:
                 item_type = 'cookie'
             falling_objects.append(Falling_Object(item_type))
 
@@ -180,24 +200,24 @@ def play_game():
             if falling_object.y_pos > SCREEN_HEIGHT:
                 falling_objects.remove(falling_object)
 
-            if cookie_monster.rect.colliderect(falling_object.image_rectangle()):
-                cookie_monster.update_score(falling_object)
+            if player.rect.colliderect(falling_object.image_rectangle()):
+                player.update_score(falling_object)
                 falling_objects.remove(falling_object)
 
 
         screen.fill((GRAY))
         
         # Draw the player's rectangle
-        screen.blit(cookie_monster.image, cookie_monster.rect)
+        screen.blit(player.image, player.rect)
         
         # draw falling objects to screen
         for item in falling_objects:
             item.render_image(screen)
         
         # Add score and top score to the screen.
-        add_text('Top Score: %s' % (cookie_monster.top_score), font, screen, SCREEN_WIDTH - 30, 20, BLUE)
-        add_text('Score: %s' % (cookie_monster.score), font, screen, SCREEN_WIDTH - 30, 70, BLUE)
-        add_text('Lives: %s' % (cookie_monster.lives), font, screen, SCREEN_WIDTH - 30, 120, BLUE)
+        add_score_text('Top Score: %s' % (player.top_score), font, screen, SCREEN_WIDTH - 30, 20, BLUE)
+        add_score_text('Score: %s' % (player.score), font, screen, SCREEN_WIDTH - 30, 70, BLUE)
+        add_score_text('Lives: %s' % (player.lives), font, screen, SCREEN_WIDTH - 30, 120, BLUE)
 
         pygame.display.update()
         game_clock.tick(60)
@@ -205,8 +225,10 @@ def play_game():
 def game_over():
     pygame.mixer.music.stop()
     pygame.mouse.set_visible(True)
+
     btn_width = 200
     btn_height = 100
+    
     play_again_btn = pygame.Rect((SCREEN_WIDTH/4) - (btn_width/2), (SCREEN_HEIGHT/2) - (btn_height/2), btn_width, btn_height)
     play_again_text_obj = font.render("PLAY AGAIN", 1, WHITE)
     play_again_text_rect = play_again_text_obj.get_rect()
@@ -217,7 +239,6 @@ def game_over():
     quit_text_rect = quit_text_obj.get_rect()
     quit_text_rect.center = quit_btn.center
 
-    
     done = False
     while not done:
         for event in pygame.event.get():
